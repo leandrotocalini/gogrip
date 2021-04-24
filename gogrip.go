6d85ca0628ea64a15648820e27bdc38b9d2e55f3
Leandro Tocalini Joerg <leandro.tocalini@veeva.com>
2021-04-24 12:21:39 +0200

    Only reading utf-8 files

--- gogrip.go
+++ gogrip.go
@@ -5,16 +5,38 @@ import (
 	"github.com/leandrotocalini/gogrip/ds"
 	"github.com/leandrotocalini/gogrip/filter"
 	"github.com/leandrotocalini/gogrip/formatter"
+	"net/http"
 	"os"
 	"path/filepath"
 )
 
+func isText(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+	buffer := make([]byte, 16)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return false
+	}
+	file.Seek(0, 0)
+
+	contentType := http.DetectContentType(buffer)
+	if contentType == "text/plain; charset=utf-8" {
+		return true
+	}
+	return false
+}
 func GetFiles(rootPath string) <-chan string {
 	filesInChan := make(chan string)
 	go func() {
 		filepath.Walk(rootPath, func(path string, file os.FileInfo, err error) error {
 			if !file.IsDir() {
-				filesInChan <- path
+				if isText(path) {
+					filesInChan <- path
+				}
 			}
 			return nil
 		})
