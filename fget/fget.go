2798cfff49167ee51b0f45a42021fcff2d5b2479
Leandro Tocalini Joerg <leandro.tocalini@veeva.com>
2021-04-24 12:58:24 +0200

    creating a package for the input files

---
+++ fget/fget.go
@@ -0,0 +1,42 @@
+package fget
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func isText(path string) bool {
+	file, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer file.Close()
+	buffer := make([]byte, 16)
+	_, err = file.Read(buffer)
+	if err != nil {
+		return false
+	}
+	file.Seek(0, 0)
+
+	contentType := http.DetectContentType(buffer)
+	if contentType == "text/plain; charset=utf-8" {
+		return true
+	}
+	return false
+}
+func Get(rootPath string) <-chan string {
+	filesInChan := make(chan string)
+	go func() {
+		filepath.Walk(rootPath, func(path string, file os.FileInfo, err error) error {
+			if !file.IsDir() {
+				if isText(path) {
+					filesInChan <- path
+				}
+			}
+			return nil
+		})
+		close(filesInChan)
+	}()
+	return filesInChan
+}
--- gogrip.go
+++ gogrip.go
@@ -2,55 +2,17 @@ package main
 
 import (
 	"flag"
+	"github.com/leandrotocalini/gogrip/fget"
 	"github.com/leandrotocalini/gogrip/filter"
 	"github.com/leandrotocalini/gogrip/formatter"
-	"net/http"
-	"os"
-	"path/filepath"
 )
-
-func isText(path string) bool {
-	file, err := os.Open(path)
-	if err != nil {
-		return false
-	}
-	defer file.Close()
-	buffer := make([]byte, 16)
-	_, err = file.Read(buffer)
-	if err != nil {
-		return false
-	}
-	file.Seek(0, 0)
-
-	contentType := http.DetectContentType(buffer)
-	if contentType == "text/plain; charset=utf-8" {
-		return true
-	}
-	return false
-}
-func GetFiles(rootPath string) <-chan string {
-	filesInChan := make(chan string)
-	go func() {
-		filepath.Walk(rootPath, func(path string, file os.FileInfo, err error) error {
-			if !file.IsDir() {
-				if isText(path) {
-					filesInChan <- path
-				}
-			}
-			return nil
-		})
-		close(filesInChan)
-	}()
-	return filesInChan
-}
 
 func main() {
 	flag.Parse()
 	query := flag.Arg(0)
 	rootPath := flag.Arg(1)
-	filesInChan := GetFiles(rootPath)
 	foundChannel := make(chan filter.Found)
-	go filter.FileInChannel(query, filesInChan, foundChannel)
+	go filter.FileInChannel(query, fget.Get(rootPath), foundChannel)
 	for elem := range foundChannel {
 		formatter.View(elem)
 	}
